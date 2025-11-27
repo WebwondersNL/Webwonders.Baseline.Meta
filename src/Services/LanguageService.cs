40562cbe0c3d1dbdde9b9cac686eef0d24287cc0
TomChanceWebwonders <tom@webwonders.nl>
2025-11-27 14:36:09 +0100

    Fixing Language service falling back incorrectly

--- src/Services/LanguageService.cs
+++ src/Services/LanguageService.cs
@@ -14,52 +14,59 @@ public class LanguageService() : ILanguageService
 {
 
     public LanguagesModel? GetLanguages(IPublishedContent currentPage, string? culture, bool fallbackToAncestors = true)
+    {
+        if (culture.IsNullOrWhiteSpace())
         {
-            if (culture.IsNullOrWhiteSpace())
-            {
-                return null;
-            }
-
-            var currentCulture = new CultureInfo(culture);
-            var result = new LanguagesModel()
-            {
-                CurrentName = currentCulture.IsNeutralCulture ? currentCulture.NativeName : currentCulture.Parent.NativeName,
-                CurrentFlagClass = new RegionInfo(currentCulture.LCID).TwoLetterISORegionName.ToLower()
-            };
-
-            var home = currentPage.Root();
-            var rootCultures = home?.Cultures.Select(x => x.Value);
-            var pageCultures = currentPage.Cultures.Select(x => x.Value).ToArray();
-            var showInLanguageMenuPropertyAlias = "showInLanguageMenu"; // We must set this manually as we don't have access to the Home model here
-            if (home != null && rootCultures != null)
-            {
-                foreach (var rootCulture in rootCultures)
-                {
-                    if (home.Value<bool>(showInLanguageMenuPropertyAlias, rootCulture.Culture))
-                    {
-                        var currentCultureInfo = new CultureInfo(rootCulture.Culture);
-                        var currentLanguageInfo = new RegionInfo(currentCultureInfo.LCID);
-
-                        var pageExistsInLanguages = !fallbackToAncestors && pageCultures.Any(x => x.Culture == rootCulture.Culture);
-                        
-                        var languageModel = new LanguageModel()
-                        {
-                            Name = currentCultureInfo.IsNeutralCulture ? currentCultureInfo.NativeName : currentCultureInfo.Parent.NativeName,
-                            FlagClass = currentLanguageInfo.TwoLetterISORegionName.ToLower(),
-                            Url = pageExistsInLanguages
-                                ? $"{currentPage.Url(rootCulture.Culture, mode: UrlMode.Absolute)}"
-                                : $"{home.Url(rootCulture.Culture, mode: UrlMode.Absolute)}",
-                            Culture = currentCultureInfo.Name
-                        };
-                        
-                        if (fallbackToAncestors || pageExistsInLanguages)
-                        {
-                            result.Languages.Add(languageModel);
-                        }
-                    }
-                }
-            }
-
-            return result;
+            return null;
         }
+
+        var currentCulture = new CultureInfo(culture);
+        var result = new LanguagesModel
+        {
+            CurrentName = currentCulture.IsNeutralCulture
+                ? currentCulture.NativeName
+                : currentCulture.Parent.NativeName,
+            CurrentFlagClass = new RegionInfo(currentCulture.LCID).TwoLetterISORegionName.ToLower()
+        };
+
+        var home = currentPage.Root();
+        var rootCultures = home?.Cultures.Select(x => x.Value);
+        var pageCultures = currentPage.Cultures.Select(x => x.Value).ToArray();
+        var showInLanguageMenuPropertyAlias = "showInLanguageMenu";
+
+        if (home != null && rootCultures != null)
+        {
+            foreach (var rootCulture in rootCultures)
+            {
+                if (!home.Value<bool>(showInLanguageMenuPropertyAlias, rootCulture.Culture))
+                    continue;
+
+                var currentCultureInfo = new CultureInfo(rootCulture.Culture);
+                var currentLanguageInfo = new RegionInfo(currentCultureInfo.LCID);
+                
+                var pageHasCulture = pageCultures.Any(x => x.Culture == rootCulture.Culture);
+                
+                var url = pageHasCulture
+                    ? currentPage.Url(rootCulture.Culture, mode: UrlMode.Absolute)
+                    : home.Url(rootCulture.Culture, mode: UrlMode.Absolute);
+                
+                if (!fallbackToAncestors && !pageHasCulture)
+                    continue;
+
+                var languageModel = new LanguageModel
+                {
+                    Name = currentCultureInfo.IsNeutralCulture
+                        ? currentCultureInfo.NativeName
+                        : currentCultureInfo.Parent.NativeName,
+                    FlagClass = currentLanguageInfo.TwoLetterISORegionName.ToLower(),
+                    Url = url,
+                    Culture = currentCultureInfo.Name
+                };
+
+                result.Languages.Add(languageModel);
+            }
+        }
+
+        return result;
+    }
 }
